517548f2a14ec90ec6929141424078cad7213d39
CrashCumber <valya.as@yandex.ru>
2024-03-02 14:33:28 +0300

    [task-2] move unused

--- src/writer.py
+++ src/writer.py
@@ -1,30 +1,10 @@
 import csv
-import time
 from io import TextIOWrapper
 from pathlib import Path
 
 from attrs import asdict
 
 from logger import logger
-
-
-def write_dataset(data_raw: dict | list, fieldnames: list, filename: str, repo_name: str):
-    dir_dataset = Path().absolute() / "datasets" / repo_name
-    dir_dataset.mkdir(exist_ok=True, parents=True)
-
-    file_path = dir_dataset / f"{filename}_{int(time.time())}.csv"
-    logger.info("Write dataset in file %s", file_path)
-
-    if isinstance(data_raw, dict):
-        data = data_raw.values()
-    else:
-        data = data_raw
-
-    with open(file_path, "w", newline="") as file_dataset:
-        writer = csv.DictWriter(file_dataset, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in data:
-            writer.writerow(row)
 
 
 class DataSetWriter(csv.DictWriter):
